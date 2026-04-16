9ab93ba00d93a668103d38734ab4c5ede6146e72
Sagar Shetty <shettysagar619@gmail.com>
2026-04-16 13:19:25 +0530

    adding production URL to CORS allowed origins

--- server/index.js
+++ server/index.js
@@ -26,7 +26,10 @@ const io = new SocketIOServer(server, {
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: [
+    "http://localhost:5173",
+    "https://chat-application-1-sooty.vercel.app"
+  ],
   credentials: true
 }));
 app.use(express.json()); // Essential for parsing req.body
